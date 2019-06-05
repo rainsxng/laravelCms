b8b96551df2056330dc7b6df1dd191e1f7413bf3
Rainsong <rainsxng@gmail.com>
2019-06-05 14:07:04 +0300

    Added soft delete to Post model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -15,7 +15,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        return view('posts.index')->with('posts', Post::all());
     }
 
     /**
@@ -89,8 +89,12 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        session()->flash('success', 'Post trashed successfully');
+
+        return redirect(route('posts.index'));
     }
 }
