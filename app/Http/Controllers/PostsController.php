f516a1f4bdf71c4bc1222f1db9c438934130814e
Rainsong <rainsxng@gmail.com>
2019-06-05 17:39:20 +0300

    Added datetimepicker and trix editor to create post view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Posts\CreatePostsRequest;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostsController extends Controller
 {
@@ -41,6 +42,7 @@ class PostsController extends Controller
             'title' => $request->title,
             'description' => $request->description,
             'content' => $request->content,
+            'published_at' => $request->published_at,
             'image'=> $image
         ]);
 
@@ -93,6 +95,7 @@ class PostsController extends Controller
     {
         $post = Post::withTrashed()->where('id',$id)->firstOrFail();
         if ($post->trashed()){
+            Storage::delete($post->image);
             $post->forceDelete();
         } else {
             $post->delete();
